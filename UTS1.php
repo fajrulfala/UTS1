62986cfa0934b8dcabefb21b590a6eba49e2d631
fajrulfala <fajrul362@gmail.com>
2017-06-05 10:13:13 +0700

    Add files via upload

---
+++ UTS1.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<center>
+ <div id="header"><h1>PENDAFTARAN PELANGGAN BARU</h1></div>
+ <tr>
+ <td colspan="2"
+ id="Data Pribadi"><h2>Data Pribadi</h2></td>
+ </tr>
+ <from action"welcomepost.php" method-"post">
+ <pre>
+ Nama Lengkap		: <input type="text" name="nama" />
+ Jenis Kelamin		: <input type="text" name="JK" />
+ Email				: <input type="text" name="email" />
+ <tr>
+  <td colspan="2"
+  id="Data Alamat Pengeriman"><h2>Data Alamat Pengiriman</h2></td>
+ </tr>
+ Nama Negara		: <input type="text" name="nama" />
+Kota			: <input type="text" name="kota" />
+Alamat			: <input type="text" name="alamat" />
+Kode Pos		: <input type="text" name="kode" />
+<tr>
+<td colspan="2"
+id="Data No Kontak"><h2>Data No Kontak</h2></td>
+</tr>
+No Telepon		: <input type="text" name="telpon" />
+No Handphone	: <input type="text" name="hp" />
+No Faksimili	: <input type="text" name="faks" />
+				<input type="submit"/ name="send" value="send"
+				
+</pre>
+</form>
+</body>
+</html>
